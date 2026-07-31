3a9ce5b7b4786f4423c0a246d8fed9bfcf7a6786
Nathan Delgado <0nathandelgado0@gmail.com>
2026-07-30 23:25:30 -0300

    feat(products): implement GET endpoint with optional name filter

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -6,7 +6,14 @@ import { ProductRepository } from "@/database/types/product-repository.js";
 export class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "Ok" });
+      const { name } = request.query;
+
+      const products = await knex<ProductRepository>("products")
+      .select()
+      .whereLike("name", `%${name ?? ""}%`)
+      .orderBy("name")
+
+      return response.json(products);
     } catch (error) {
       next(error);
     }
