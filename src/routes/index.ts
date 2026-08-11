c7ddfac7d280d777af02537cdfca3e943db2e7c3
Nathan Delgado <0nathandelgado0@gmail.com>
2026-08-10 23:39:50 -0300

    feat(tables-sessions): add POST route and controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 
 import { productsRoutes } from "./products-routes.js";
+import { tablesRoutes } from "./tables-routes.js";
+import { tablesSessionsRoutes } from "./tables-sessions-routes.js";
 
 const routes = Router();
-routes.use("/products", productsRoutes)
+routes.use("/products", productsRoutes);
+routes.use("/tables", tablesRoutes);
+routes.use("/tables-sessions", tablesSessionsRoutes);
 
-export { routes }
+export { routes };
