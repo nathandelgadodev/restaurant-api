0121c7a00957881c1104fc3414039a1dfd5941d2
Nathan Delgado <0nathandelgado0@gmail.com>
2026-08-12 23:41:01 -0300

    feat(orders): add GET route to list orders by session

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -52,4 +52,43 @@ export class OrdersController {
       next(error);
     }
   }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const paramSchema = z.object({
+        table_session_id: z.string().transform((value) => Number(value)),
+      });
+      const { table_session_id } = paramSchema.parse(request.params);
+
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .where({
+          id: table_session_id,
+        })
+        .first();
+
+      if (!session) {
+        throw new AppError("session table not found");
+      }
+
+      const order = await knex("orders")
+        .select(
+          "orders.id",
+          "orders.table_session_id",
+          "orders.product_id",
+          "products.name",
+          "orders.price",
+          "orders.quantity",
+          knex.raw("(orders.price * orders.quantity) AS total"),
+          "orders.created_at",
+          "orders.updated_at",
+        )
+        .join("products", "products.id", "orders.product_id")
+        .where({ table_session_id })
+        .orderBy("orders.created_at", "desc");
+
+      return response.json(order);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
